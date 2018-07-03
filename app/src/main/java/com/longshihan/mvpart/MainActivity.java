51eed3f27856121efde65b538ebee6fa2d79b6a7
longhe <LONGHE001@huazhu.com>
2018-07-03 18:22:20 +0800

    FIX:添加ARCH

--- app/src/main/java/com/longshihan/mvpart/MainActivity.java
+++ app/src/main/java/com/longshihan/mvpart/MainActivity.java
@@ -1,13 +1,46 @@
 package com.longshihan.mvpart;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.longshihan.mvpart.arch.model.User;
+import com.longshihan.mvpart.arch.viewmodel.MyViewModel0;
+import com.longshihan.mvpart.arch.viewmodel.MyViewModel1;
+import com.longshihan.mvpcomponent.*;
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.main_txt);
+        if (com.longshihan.mvpcomponent.BuildConfig.TDEBUG) {   // 这两行必须写在init之前，否则这些配置在init过程中将无效
+            Logger.addLogAdapter(new AndroidLogAdapter());//debug下使用Logger日志模块
+        }
+        final MyViewModel0 myViewModel0= ViewModelProviders.of(this).get(MyViewModel0.class);
+        myViewModel0.getUsers().observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(@Nullable User users) {
+                Logger.d(users.name);
+            }
+        });
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myViewModel0.setUsers(new User("123"));
+            }
+        });
     }
 }
