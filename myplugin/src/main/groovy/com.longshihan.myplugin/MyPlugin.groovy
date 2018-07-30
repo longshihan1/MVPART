828f4a5c72abc34fb9aa5382bc9f5c8b4ebd83ab
longshihan <577093937@qq.com>
2018-07-30 08:20:50 +0800

    添加插件

---
+++ myplugin/src/main/groovy/com.longshihan.myplugin/MyPlugin.groovy
@@ -0,0 +1,13 @@
+package com.longshihan.myplugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class MyPlugin implements Plugin<Project> {
+
+    void apply(Project project) {
+        System.out.println("========================")
+        System.out.println("start gradle plugin!")
+        System.out.println("========================")
+    }
+}
